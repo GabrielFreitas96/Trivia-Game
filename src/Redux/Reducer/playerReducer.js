e43751e952eaf844c3b3d5b0e80f65f05b54dff8
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-03-09 15:42:54 -0300

    Iniciando o projeto

---
+++ src/Redux/Reducer/playerReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+  name: '',
+  assertions: '',
+  score: '',
+  gravatarEmail: '',
+}
+
+const playerReducer = (state= INITIAL_STATE, action) => state;
+
+export default playerReducer;
