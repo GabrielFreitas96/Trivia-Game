5c303186604a8a0a3040d613568b93e7a299b5d2
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-03-10 14:36:12 -0300

    Requisito 5 - Parcial

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import logo from '../trivia.png';
+import { connect } from 'react-redux';
 import '../App.css';
+import { getGravatar } from '../Service/service'
 
-export default class Header extends Component {
+class Header extends Component {
+  state = {
+    gravatarURL: '',
+  }
+
+  async componentDidMount() {
+    const { gravatarEmail } = this.props;
+    const result = await getGravatar(gravatarEmail);
+    this.setState({ gravatarURL: result})
+  }
+
   render() {
+    const { name } = this.props;
+    const { gravatarURL } = this.state;
+    console.log(gravatarURL)
     return (
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+      <header>
+        <h4 data-testid="header-player-name" >{ name }</h4>
+        <img data-testid="header-profile-picture" src={ gravatarURL } alt="GravatarImg" />
+        <p data-testid="header-score">score: 0</p>
       </header>
     );
   }
 }
+
+const mapStateToProps = (globalState) => ({
+  gravatarEmail: globalState.players.gravatarEmail,
+  name: globalState.players.name,
+})
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Game extends React.Component {
   render() {
     return (
       <section>
-        <h1>Tela do Jogo</h1>
+        <Header />
       </section>);
   }
 }
