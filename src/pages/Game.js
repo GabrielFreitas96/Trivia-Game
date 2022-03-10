c786499b3014ec8e64389733888fff4045cfde1c
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-03-10 16:59:51 -0300

    merge requisito 5 incompleto

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import Header from '../components/Header';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getQuestions } from '../Service/service';
+// import Header from '../components/Header';
 
 class Game extends React.Component {
+/*   state = {
+    resultQuestions: [],
+  };
+ */
+  async componentDidMount() {
+    const { token } = this.props;
+    console.log(token);
+    const resultsQuestions = await getQuestions(token);
+    console.log('Result Questions', resultsQuestions);
+    /*  this.setState({
+      resultQuestions,
+    }); */
+  }
+
   render() {
+    // const { resultQuestions } = this.state;
     return (
       <section>
-        <Header />
+        <h1>Tela do Jogo</h1>
       </section>);
   }
 }
 
-export default Game;
+const mapStateToProps = (globalState) => ({
+  token: globalState.token,
+});
+
+export default connect(mapStateToProps)(Game);
+Game.propTypes = {
+  token: PropTypes.string.isRequired,
+};
