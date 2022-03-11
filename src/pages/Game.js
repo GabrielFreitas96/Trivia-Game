a5f2f83fa09883a700b4dc08517495116e2f0762
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-03-11 11:55:11 -0300

    Requisito 5 feito. Requisito 6 veio de brinde

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -8,6 +8,8 @@ class Game extends React.Component {
   state = {
     resultsQuestions: [],
     counter: 0,
+    allQuestions: [],
+    correctAnswer: '',
   }
 
   async componentDidMount() {
@@ -33,44 +35,107 @@ class Game extends React.Component {
     //     return 0;
     //   }
     // });
-    console.log('Result Questions', array);
+    // console.log('Result Questions', array);
+    const { counter } = this.state;
     this.setState({
-      resultsQuestions: array,
+      resultsQuestions: ApiResult.results,
+      correctAnswer: ApiResult.results[counter].correct_answer,
+      allQuestions: [...ApiResult.results[counter].incorrect_answers,
+        ApiResult.results[counter].correct_answer],
     });
   }
+  // https://pt.stackoverflow.com/questions/406037/mostrar-elementos-de-um-array-em-ordem-aleat%C3%B3ria usado para fazer a função ArrayRandomOrder
+
+  arrayRandomOrder = (array) => {
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  createQuestions = (arrayAllQuestions, correctAnswer) => {
+    const randomQuestions = this.arrayRandomOrder(arrayAllQuestions);
+    console.log('random Questions', randomQuestions);
+    let counterIndex = 0 - 1;
+    return randomQuestions.map((element) => {
+      if (element === correctAnswer) {
+        console.log('Entrou no IF');
+        console.log(element);
+        // const retorno = (
+        //   <button
+        //     type="button"
+        //     data-testid="correct-answer"
+        //     onClick={ () => {} }
+        //   >
+        //     {element}
+        //   </button>);
+        return (
+          <button
+            type="button"
+            data-testid="correct-answer"
+            onClick={ () => { this.clickAnswer(element); } }
+          >
+            {element}
+          </button>);
+      }
+      counterIndex += 1;
+      console.log(counterIndex);
+      console.log('Não entrou no if');
+      console.log(element);
+      return (
+        <button
+          type="button"
+          data-testid={ `wrong-answer-${counterIndex}` }
+          onClick={ () => { this.clickAnswer(element); } }
+          key={ counterIndex }
+        >
+          {element}
+        </button>);
+    });
+  };
+
+  clickAnswer = (answer) => {
+    console.log('Clicado');
+    console.log('Clicou na Answer', answer);
+  };
+
+  clickNextQuestion = () => {
+    console.log('Clicou Next Question');
+    this.setState((prevState) => (
+      { counter: prevState.counter + 1 }
+    ));
+  };
 
   render() {
-    const { resultsQuestions, counter } = this.state;
+    const { resultsQuestions, counter, correctAnswer, allQuestions } = this.state;
     return (
       <section>
         <Header />
         { resultsQuestions.length === 0 ? null
           : (
             <div>
-              <p
+              <h2
                 data-testid="question-category"
               >
                 { resultsQuestions[counter].category }
-              </p>
-              <p data-testid="question-text">{ resultsQuestions[counter].question }</p>
+              </h2>
+              <h3
+                data-testid="question-text"
+              >
+                { resultsQuestions[counter].question }
+              </h3>
               <div data-testid="answer-options">
-                <p
-                  data-testid="correct-answer"
-                >
-                  { resultsQuestions[counter].correct_answer }
-                </p>
-                { resultsQuestions[counter]
-                  .incorrect_answers.map((element, index) => (
-                    <p
-                      key={ index }
-                      data-testid={ `wrong-answer-${index}` }
-                    >
-                      { element }
-                    </p>
-                  ))}
+                {this.createQuestions(allQuestions, correctAnswer)}
               </div>
             </div>
           )}
+        <button
+          type="button"
+          onClick={ () => { this.clickNextQuestion(); } }
+        >
+          NEXT QUESTION
+        </button>
       </section>
     );
   }
