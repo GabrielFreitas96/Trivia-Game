f496ad47176db79e2c416c21166caaf23371348a
fernando <pcabral.fernando@gmail.com>
2022-03-14 15:56:11 -0300

    requisito 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
     return (
-      <section>
-        <h1 data-testid="feedback-text">Página de Feedback</h1>
-        <Header />
-      </section>
+      <div>
+        <section>
+          <h1 data-testid="feedback-text">Página de Feedback</h1>
+          <Header />
+        </section>
+        <Link
+          to="/"
+        >
+          <button
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Play again
+          </button>
+        </Link>
+      </div>
     );
   }
 }
