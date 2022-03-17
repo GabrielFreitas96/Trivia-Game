7c9babca4bfbb099ad1b662eb60d1bafdbb58778
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-03-17 16:03:29 -0300

    Updates

---
+++ precache-manifest.c2ac993f6ed084f198bee563fa3c5b27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6973c84df52260514d68f47e00a753c1",
+    "url": "/Trivia-Game/index.html"
+  },
+  {
+    "revision": "63ed92ebf7ee86e07e3a",
+    "url": "/Trivia-Game/static/css/main.8747f293.chunk.css"
+  },
+  {
+    "revision": "12165afbf2d9972a5b8b",
+    "url": "/Trivia-Game/static/js/2.69241785.chunk.js"
+  },
+  {
+    "revision": "6b427be26341f18e5beeadca3932038e",
+    "url": "/Trivia-Game/static/js/2.69241785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ed92ebf7ee86e07e3a",
+    "url": "/Trivia-Game/static/js/main.d3ad0bcd.chunk.js"
+  },
+  {
+    "revision": "6670c406db93b18b5d1e",
+    "url": "/Trivia-Game/static/js/runtime-main.2e0d12fd.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia-Game/static/media/trivia.466d153e.png"
+  }
+]);
