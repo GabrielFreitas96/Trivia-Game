d7eeabd4094733b1c5f00e5169e19c41bfc1b520
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-03-18 13:10:02 -0300

    Atualizando

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getCategorys } from '../Service/service';
 import { CategoryAction, DifficultyAction, TypeAction } from '../Redux/Actions/index';
+import '../style/Settings.css';
 
 class Settings extends React.Component {
     state = {
@@ -18,54 +19,57 @@ class Settings extends React.Component {
   createSelectCategory = (categorys) => {
     console.log(categorys);
     return (
-    <select onChange={this.handleCategoryChange}>
-      { categorys.map(({id, name}) => (
-        <option name={id} value={id}>{name}</option>
-      ))};
-    </select>
-    )
-  }
-
-  handleCategoryChange = ({target}) => {
-  const { dispatch } = this.props;
-  dispatch(CategoryAction(target.value));
-  }
-
-  handleDifficultyChange = ({target}) => {
-    const { dispatch } = this.props;
-    dispatch(DifficultyAction(target.value));
-    }
-
-    handleTypeChange = ({target}) => {
-      const { dispatch } = this.props;
-      dispatch(TypeAction(target.value));
-      }
-  
-  render() {
-    const { categorys } = this.state;
-    return (
-      <>
-        <h1 className="settings-h1">Settings</h1>
-        { categorys.length !== 0 ? this.createSelectCategory(categorys)
-          : null
-        }
-          <select onChange={ this.handleDifficultyChange}>
-            <option value="">Any Difficulty</option>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-          </select>
-          <select onChange={ this.handleTypeChange}>
-            <option value="">Any Type</option>
-            <option value="multiple">Multiple Choice</option>
-            <option value="boolean">True/False</option>
-          </select>
-        <Link to='/game'>
-            <button type="button">Play</button>
-          </Link>
-      </>
+      <select className="settings-list" onChange={ this.handleCategoryChange }>
+        { categorys.map(({ id, name }, index) => (
+          <option key={ index } name={ id } value={ id }>{name}</option>
+        ))}
+      </select>
     );
   }
+
+  handleCategoryChange = ({ target }) => {
+    const { dispatch } = this.props;
+    dispatch(CategoryAction(target.value));
+  }
+
+  handleDifficultyChange = ({ target }) => {
+    const { dispatch } = this.props;
+    dispatch(DifficultyAction(target.value));
+  }
+
+    handleTypeChange = ({ target }) => {
+      const { dispatch } = this.props;
+      dispatch(TypeAction(target.value));
+    }
+
+    render() {
+      const { categorys } = this.state;
+      return (
+        <>
+          <h1 className="settings-h1">Settings</h1>
+          <section className="settings-section">
+            <div className="settings-div">
+              { categorys.length !== 0 ? this.createSelectCategory(categorys)
+                : null }
+              <select className="settings-list" onChange={ this.handleDifficultyChange }>
+                <option value="">Any Difficulty</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+              <select className="settings-list" onChange={ this.handleTypeChange }>
+                <option value="">Any Type</option>
+                <option value="multiple">Multiple Choice</option>
+                <option value="boolean">True/False</option>
+              </select>
+              <Link to="/game">
+                <button className="settings-play" type="button">Play</button>
+              </Link>
+            </div>
+          </section>
+        </>
+      );
+    }
 }
 
 export default connect()(Settings);
